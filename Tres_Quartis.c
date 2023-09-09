b89c90f3d7ea889efa279b5fe5252d1982e1f8d5
GabrielFM07 <gmfontenele@hotmail.com>
2023-09-08 22:54:40 -0300

    Update v2.0 Beta
    Implementação de uma interface gráfica arcaica e 2 novas funcionalidades, mais um sneak peak de 2 outras à ser implementadas.
    
    Co-Authored-By: João Emanuel Nery da Silva <120668067+joaoemanuel04@users.noreply.github.com>

--- Tres_Quartis.c
+++ Tres_Quartis.c
@@ -3,8 +3,36 @@
 #include <locale.h>
 #include <math.h>
 
-int ORD(float *vetor, int tamanho)
-{
+int menu(){
+	int esc;
+	
+	printf("\tInforme qual função do programa você deseja utilizar:\n ___________________________________________________________________\n|\t\t\t\t\t\t\t\t    |\n");
+	printf("|1-<Três Quartis>\t2-<Média Aritimética>\t 3-<Média Ponderada>|\n|\t\t\t\t\t\t\t\t    |\n|4-<Distribuição de Frequência>\t       5-<Variância e Desvio Padrão>|\n");
+	printf("|___________________________________________________________________|\n");
+	scanf("%d",&esc);
+	
+	return esc;
+}
+/*void ORDP(float* vetor,float *peso,int tamanho){
+	int i, j;
+    float temp,temp2;
+    for ( i = 0; i < tamanho; i++)
+    {
+        for ( j = i; j < tamanho; j++)
+        {
+            if (vetor[i] > vetor[j])
+            {
+                temp = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = temp;
+                temp2 = peso[i];
+                peso[i] = peso[j];
+                peso[j] = temp2;
+             }
+        }
+	}
+}À ser usado no futuro. (escrever função assim: 'ORDP(&v,&p,n)'*/
+float* ORD(float *vetor, int tamanho){
     int i, j;
     float temp;
     for ( i = 0; i < tamanho; i++)
@@ -21,7 +49,6 @@ int ORD(float *vetor, int tamanho)
 	}
 	return vetor;
 }
-
 void OUTL(float *vet,float ol,int n){
 	int i;
 	printf("São outliers à esquerda:\n");
@@ -73,79 +100,126 @@ float AIQ(float q1,float q3){
 	res=q3-q1;
 	return res;
 }
-/*void MED(float *vet,float *freq,float outl,float outr,int n){
-	float res,reso,soma,somao,somaf;
+void MEDA(float *vet,int n){
+	float res,soma;
 	int i;
 	soma=0;
-	somao=0;
-	somaf=0;
 	for(i=0;i<n;i++){
-		somaf+=freq[i];
+		soma+=vet[i];
 	}
+	res=soma/n;
+	printf("A média aritmética do conjunto de dados analisado é: %.2f\n",res);
+}
+void MEDP(float *vet,float *pes,int n){
+	float res,soma,somap;
+	int i;
+	soma=0;
+	somap=0;
 	for(i=0;i<n;i++){
-		if(vet[i]>outl && vet[i]<outr){
-		soma+=(vet[i]*freq[i]);
-		}
+		soma+=(vet[i]*pes[i]);
+		somap+=pes[i];
 	}
-	res=soma/somaf;
-	printf("A média do conjunto de dados analisados (ignorando os outliers) é: %.2f\n",res);
-	for(i=0;i<n;i++){
-		somao+=(vet[i]*freq[i]);
-	}
-	reso=somao/somaf;
-	printf("A média do conjunto de dados analisados (contando com os outliers) é: %.2f\n",reso);
-}*/
+	res=soma/somap;
+	printf("A média ponderada do conjunto de dados analisado é: %.2f\n",res);
+}
 
 int main()
 {
 	float aiq,q1,q2,q3,tq,ol,or;
-	int i,n;
-	float *v;
-	//float *f;
-	float *v2;
+	int i,n,esc;
+	float *v,*v2;
+	float *p;
+	//float *p2;
 	
 	setlocale(LC_ALL,"Portuguese");
-	
-	printf("Esse programa analisará um conjunto de dados por meio dos Três Quartis.\n");
 	do{
-		printf("Informe a quantidade de dados a ser analisada: ");
-		scanf("%d",&n);
-		if(n<0){
-			printf("A quantidade informada é inválida, por favor digite um valor maior que 0.\n");
+		esc=menu();
+		if(esc==1){
+			system("CLS");
+			printf("\nEsse programa analisará um conjunto de dados por meio dos Três Quartis.\n");
+			do{
+				printf("Informe a quantidade de dados a ser analisada: ");
+				scanf("%d",&n);
+				if(n<0){
+					printf("A quantidade informada é inválida, por favor digite um valor maior que 0.\n");
+				}
+			}while(n<0);
+			v = (float*)malloc(sizeof(int)*n);
+			v2 = (float*)malloc(sizeof(int)*n);
+			
+			for(i=0;i<n;i++){
+				printf("Informe o %dº dado: ",i+1);
+				scanf("%f",&v[i]);
+			}
+			tq=TRQR(n);
+			v2=ORD(v,n);
+			q1=Q1(v2,n);
+			q2=Q2(v2,n);
+			q3=Q3(v2,n);
+			aiq=AIQ(q1,q3);
+			ol=q1-1.5*aiq;
+			or=q3+1.5*aiq;
+			printf("\nResolução:\nOs quartis se encontram a uma distância aproximada de %.0f dados um do outro.\n",tq);
+			printf("O 1º quartil é: %.2f\n",q1);
+			printf("O 2º quartil é: %.2f\n",q2);
+			printf("O 3º quartil é: %.2f\n",q3);
+			printf("O valor da amplitude interquartil é: %2.f\n", aiq);
+			OUTL(v,ol,n);
+			printf("\n");
+			OUTR(v,or,n);
+			printf("\nOBS: Caso nenhum valor seja informado é porque não há outliers.\n");
 		}
-	}while(n<0);
+		if(esc==2){
+			system("CLS");
+			printf("\nEsse programa analisará um conjunto de dados e informará a média aritimética deles.\n");
+			do{
+				printf("Informe a quantidade de dados a ser analisada: ");
+				scanf("%d",&n);
+				if(n<0){
+					printf("A quantidade informada é inválida, por favor digite um valor maior que 0.\n");
+				}
+			}while(n<0);
+			v = (float*)malloc(sizeof(int)*n);
+			v2 = (float*)malloc(sizeof(int)*n);
+			
+			for(i=0;i<n;i++){
+				printf("Informe o %dº dado: ",i+1);
+				scanf("%f",&v[i]);
+			}
+			v2=ORD(v,n);
+			MEDA(v2,n);
+		}
+		if(esc==3){
+			system("CLS");
+			printf("Esse programa analisará um conjunto de dados e informará a média ponderada deles.\n");
+			do{
+				printf("Informe a quantidade de dados a ser analisada: ");
+				scanf("%d",&n);
+				if(n<0){
+					printf("A quantidade informada é inválida, por favor digite um valor maior que 0.\n");
+				}
+			}while(n<0);
 	
-	v = (float*)malloc(sizeof(int)*n);
-	//f = (float*)malloc(sizeof(int)*n);
-	v2 = (float*)malloc(sizeof(int)*n);
+			v = (float*)malloc(sizeof(int)*n);
+			p = (float*)malloc(sizeof(int)*n);
+			v2 = (float*)malloc(sizeof(int)*n);
 	
-	//printf("Por favor, informe o valor dos dados.\n");
-	for(i=0;i<n;i++){
-		printf("Informe o %dº dado: ",i+1);
-		scanf("%f",&v[i]);
-		//printf("Informe a frequência em que o %dº dado aparece: ",i+1);
-		//scanf("%f",&f[i]);
-	}
-	
-	tq=TRQR(n);
-	v2=ORD(v,n);
-	q1=Q1(v2,n);
-	q2=Q2(v2,n);
-	q3=Q3(v2,n);
-	aiq=AIQ(q1,q3);
-	ol=q1-1.5*aiq;
-	or=q3+1.5*aiq;
-	
-	printf("\nOs quartis se encontram a uma distância aproximada de %.2f dados um do outro.\n",tq);
-	printf("O 1º quartil é: %.2f\n",q1);
-	printf("O 2º quartil é: %.2f\n",q2);
-	printf("O 3º quartil é: %.2f\n",q3);
-	printf("O valor da amplitude interquartil é: %2.f\n", aiq);
-	OUTL(v,ol,n);
-	printf("\n");
-	OUTR(v,or,n);
-	printf("\nOBS: Caso nenhum valor seja informado é porque não há outliers.\n");
-	//MED(v,f,ol,or,n);
+			printf("Por favor, informe o valor dos dados.\n");
+			for(i=0;i<n;i++){
+				printf("Informe o %dº dado: ",i+1);
+				scanf("%f",&v[i]);
+				printf("Informe o peso do %dº dado: ",i+1);
+				scanf("%f",&p[i]);
+			}
+			MEDP(v,p,n);
+		}
+		if(esc==4){
+			printf("\tEssa função será implementada em versões futuras.\n");
+		}
+		if(esc==5){
+			printf("\t3Essa função será implementada em versões futuras.\n");
+		}
+	}while(esc<1 || esc>5);
 	
 	system("PAUSE");
 	
