40c9d7b8246dd45dd71db6f0bf0f8d5ae494da62
GabrielFM07 <gmfontenele@hotmail.com>
2023-09-11 01:35:33 -0300

    Update v2.0

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Changelog:
 1.2.0.0: Adição de um condicional para consertar um bug que acontecia caso a quantidade de elementos fosse 5;
 1.3.0.0: Implementação de um sistema de ordenação automática por joaoemanuel04, remoção do cálculo de médias ponderadas por enquanto (buscamos reimplementar essa funcionalidade e mais outras em uma versão futura) e correções/melhorias de algumas funções anteriormente inconsistentes.
 2.0.0.0 Beta: Implementação de uma interface gráfica arcaica e duas novas funcionalidades, mais um sneak peak de outras duas funcionalidades que ainda serão implementadas.
+2.0.0.0: Melhorias na interface gráfica, melhorias/simplificações de sintaxe ao longo de todo o código, implementação das funções de Variância & Desvio Padrão e Distribuição de Frequência.
--- Tres_Quartis.c
+++ Tres_Quartis.c
@@ -7,31 +7,26 @@ int menu(){
 	int esc;
 	
 	printf("\tInforme qual função do programa você deseja utilizar:\n ___________________________________________________________________\n|\t\t\t\t\t\t\t\t    |\n");
-	printf("|1-<Três Quartis>\t2-<Média Aritimética>\t 3-<Média Ponderada>|\n|\t\t\t\t\t\t\t\t    |\n|4-<Distribuição de Frequência>\t       5-<Variância e Desvio Padrão>|\n");
-	printf("|___________________________________________________________________|\n");
+	printf("| 1-<Três Quartis>\t\t 2-<Média Populacional ou Amostral> |\n|\t\t\t\t\t\t\t\t    |\n| 3-<Média Ponderada>\t\t     4-<Distribuição de Frequência> |\n");
+	printf("|\t\t\t\t\t\t\t\t    |\n| 5-<Variância e Desvio Padrão>\t\t\t\t   6-<Sair> |\n|___________________________________________________________________|\n");
 	scanf("%d",&esc);
 	
 	return esc;
 }
-/*void ORDP(float* vetor,float *peso,int tamanho){
-	int i, j;
-    float temp,temp2;
-    for ( i = 0; i < tamanho; i++)
-    {
-        for ( j = i; j < tamanho; j++)
-        {
-            if (vetor[i] > vetor[j])
-            {
-                temp = vetor[i];
-                vetor[i] = vetor[j];
-                vetor[j] = temp;
-                temp2 = peso[i];
+float* ORDP(float *vetor,float *peso,int n){
+	int i,j;
+    float temp;
+    for(i=0;i<n;i++){
+        for(j=i;j<n;j++){
+            if(vetor[i]>vetor[j]){
+                temp = peso[i];
                 peso[i] = peso[j];
-                peso[j] = temp2;
+                peso[j] = temp;
              }
         }
 	}
-}À ser usado no futuro. (escrever função assim: 'ORDP(&v,&p,n)'*/
+	return peso;
+}
 float* ORD(float *vetor, int tamanho){
     int i, j;
     float temp;
@@ -80,13 +75,10 @@ float Q1(float *vet,int n){
 float Q2(float *vet,int n){
 	float res;
 	if(n%2==0){
-		res=(vet[((3*n)/4)-(n/4)-1]+vet[((3*n)/4)-(n/4)])/2;
+		res=(vet[n/2]+vet[n/2-1])/2;
 	}
-	if(n%2!=0 && n!=5){
-		res=vet[((3*n)/4)-(n/4)-1];
-	}
-	if(n%2!=0 && n==5){
-		res=vet[((3*n)/4)-(n/4)];
+	else{
+		res=vet[n/2];
 	}
 	return res;
 }
@@ -101,14 +93,31 @@ float AIQ(float q1,float q3){
 	return res;
 }
 void MEDA(float *vet,int n){
-	float res,soma;
-	int i;
+	float res,soma,mdn,mde;
+	int i,j,cont,val;
+	val=0;
 	soma=0;
 	for(i=0;i<n;i++){
 		soma+=vet[i];
+		cont=0;
+		for(j=0;j<n;j++){
+			if(vet[j]==vet[i]){
+				++cont;
+			}
+		}
+		if(cont>val){
+			val=cont;
+			mde=vet[i];
+		}
 	}
 	res=soma/n;
-	printf("A média aritmética do conjunto de dados analisado é: %.2f\n",res);
+	if(n%2==0){
+		mdn=(vet[n/2]+vet[n/2-1])/2;
+	}
+	else{
+		mdn=vet[n/2];
+	}
+	printf("A média aritmética do conjunto de dados analisado é: %.2f\nA mediana do conjunto de dados é: %.2f\nA moda do conjunto de dados é: %.2f\n",res,mdn,mde);
 }
 void MEDP(float *vet,float *pes,int n){
 	float res,soma,somap;
@@ -122,17 +131,57 @@ void MEDP(float *vet,float *pes,int n){
 	res=soma/somap;
 	printf("A média ponderada do conjunto de dados analisado é: %.2f\n",res);
 }
+void VDP(float *vet,int n){
+	float med,var,soma1,soma2;
+	int i;
+	soma1=0;
+	soma2=0;
+	for(i=0;i<n;i++){
+		soma1+=vet[i];
+	}
+	med=soma1/n;
+	for(i=0;i<n;i++){
+		if(vet[i]>med){
+			soma2+=(vet[i]-med)*(vet[i]-med);
+		}
+		if(vet[i]<med){
+			soma2+=(med-vet[i])*(med-vet[i]);
+		}
+	}
+	var=soma2/n;
+	printf("A média aritmética do conjunto de dados analisado é: %.2f\nA variância do conjunto de dados analisado é: %.2f\nO desvio padrão do conjunto de dados analisado é: %.2f\n",med,var,sqrtf(var));
+}
+void DF(float *vet,float *pes,int n){
+	int i;
+	float fi,fr,Fri;
+	fi=0,Fri=0;
+	for(i=0;i<n;i++){
+		fi+=pes[i];
+	}
+	for(i=0;i<n;i++){
+		fr=0;
+		fr+=(pes[i]/fi)*100;
+		printf("A frequência relativa do %dº dado é de %.2f%%\n",i+1,fr);
+	}
+	for(i=0;i<n;i++){
+		Fri+=(pes[i]/fi)*100;
+		printf("A frequência relativa acumulada do %dº dado é de %.2f%%\n",i+1,Fri);
+	}
+	printf("A frequência absoluta do conjunto de dados analisado é: %.2f\n",fi);
+	
+}
 
 int main()
 {
 	float aiq,q1,q2,q3,tq,ol,or;
-	int i,n,esc;
+	int i,n,esc,esc2;
 	float *v,*v2;
 	float *p;
-	//float *p2;
+	float *p2;
 	
 	setlocale(LC_ALL,"Portuguese");
 	do{
+		system("CLS");
 		esc=menu();
 		if(esc==1){
 			system("CLS");
@@ -168,6 +217,8 @@ int main()
 			printf("\n");
 			OUTR(v,or,n);
 			printf("\nOBS: Caso nenhum valor seja informado é porque não há outliers.\n");
+			system("PAUSE");
+			continue;
 		}
 		if(esc==2){
 			system("CLS");
@@ -179,15 +230,16 @@ int main()
 					printf("A quantidade informada é inválida, por favor digite um valor maior que 0.\n");
 				}
 			}while(n<0);
+			
 			v = (float*)malloc(sizeof(int)*n);
-			v2 = (float*)malloc(sizeof(int)*n);
 			
 			for(i=0;i<n;i++){
 				printf("Informe o %dº dado: ",i+1);
 				scanf("%f",&v[i]);
 			}
-			v2=ORD(v,n);
-			MEDA(v2,n);
+			MEDA(v,n);
+			system("PAUSE");
+			continue;
 		}
 		if(esc==3){
 			system("CLS");
@@ -202,9 +254,7 @@ int main()
 	
 			v = (float*)malloc(sizeof(int)*n);
 			p = (float*)malloc(sizeof(int)*n);
-			v2 = (float*)malloc(sizeof(int)*n);
 	
-			printf("Por favor, informe o valor dos dados.\n");
 			for(i=0;i<n;i++){
 				printf("Informe o %dº dado: ",i+1);
 				scanf("%f",&v[i]);
@@ -212,14 +262,83 @@ int main()
 				scanf("%f",&p[i]);
 			}
 			MEDP(v,p,n);
+			system("PAUSE");
+			continue;
 		}
 		if(esc==4){
-			printf("\tEssa função será implementada em versões futuras.\n");
+			system("CLS");
+			printf("Esse programa analisará um conjunto de dados e realizará uma distribuição de frequência no mesmo.\n");
+			do{
+				printf("Informe a quantidade de dados a ser analisada: ");
+				scanf("%d",&n);
+				if(n<0){
+					printf("A quantidade informada é inválida, por favor digite um valor maior que 0.\n");
+				}
+			}while(n<0);
+	
+			v = (float*)malloc(sizeof(int)*n);
+			v2 = (float*)malloc(sizeof(int)*n);
+			p = (float*)malloc(sizeof(int)*n);
+			p2 = (float*)malloc(sizeof(int)*n);
+	
+			for(i=0;i<n;i++){
+				printf("Informe o %dº dado: ",i+1);
+				scanf("%f",&v[i]);
+				printf("Informe a frequência em que o %dº dado aparece: ",i+1);
+				scanf("%f",&p[i]);
+			}
+			v2=ORD(v,n);
+			p2=ORDP(v,p,n);
+			do{
+				printf("Você deseja limpar a tela antes de receber os resultados?\n(1-Sim  /  2-Não)\n");
+				scanf("%d",&esc2);
+				if(esc2==1){
+					system("CLS");
+				}
+				if(esc2==1){
+					continue;
+				}
+				else{
+					printf("Valor inválido, por favor digite um valor entre 1 e 2.\n");
+					continue;
+				}
+			}while(esc2 !=1 && esc2 !=2);
+			DF(v2,p2,n);
+			
+			system("PAUSE");
+			continue;
 		}
 		if(esc==5){
-			printf("\t3Essa função será implementada em versões futuras.\n");
+			system("CLS");
+			printf("Esse programa analisará um conjunto de dados e informará a média, variância e desvio padrão deles.\n");
+			do{
+				printf("Informe a quantidade de dados a ser analisada: ");
+				scanf("%d",&n);
+				if(n<0){
+					printf("A quantidade informada é inválida, por favor digite um valor maior que 0.\n");
+				}
+			}while(n<0);
+			
+			v = (float*)malloc(sizeof(int)*n);
+			
+			for(i=0;i<n;i++){
+				printf("Informe o %dº dado: ",i+1);
+				scanf("%f",&v[i]);
+			}
+			VDP(v,n);
+			system("PAUSE");
+			continue;
 		}
-	}while(esc<1 || esc>5);
+		if(esc==6){
+			printf("\t\t\t\tSaindo...\n");
+			exit(0);
+		}
+		if(esc<1 || esc>6){
+			printf("Valor inválido, por favor digite um valor entre 1 e 6.\n");
+			system("PAUSE");
+			continue;
+		}
+	}while(esc!=6);
 	
 	system("PAUSE");
 	
